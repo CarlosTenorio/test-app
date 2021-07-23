ee71b0e89da0736e7ef9f0da3f645dade68c0e27
Carlos Tenorio Pérez <carlos.tenorio@libertyseguros.es>
2021-07-23 10:16:36 +0200

    Add book preview with material

--- src/app/modules/books/books.module.ts
+++ src/app/modules/books/books.module.ts
@@ -5,12 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { CollectionPageComponent } from './containers/collection-page/collection-page.component';
 import { BookPreviewComponent, BookPreviewListComponent } from './components';
 import { BookService } from './services';
+import { MatCardModule } from '@angular/material/card';
 
 const childRoutes: Route[] = [{ path: '', component: CollectionPageComponent }];
 
 @NgModule({
   declarations: [CollectionPageComponent, BookPreviewComponent, BookPreviewListComponent],
-  imports: [CommonModule, RouterModule.forChild(childRoutes), HttpClientModule],
+  imports: [CommonModule, RouterModule.forChild(childRoutes), HttpClientModule, MatCardModule],
   providers: [BookService],
 })
 export class BooksModule {}
